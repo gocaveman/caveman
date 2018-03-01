e583623dad420234a1ebdd4fd68fbf395f8a686b
Brad Peabody <brad@peabody.io>
2018-03-01 15:14:40 -0800

    test for basic auth

---
+++ webutil/http-basic-auth-handler_test.go
@@ -0,0 +1,42 @@
+package webutil
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestBasicAuthHandler(t *testing.T) {
+
+	assert := assert.New(t)
+
+	h := NewBasicAuthOneHandler("joe", "secret")
+	var hl HandlerList
+	hl = append(hl, NewContextCancelHandler())
+	hl = append(hl, h)
+	hl = append(hl, http.NotFoundHandler())
+
+	s := httptest.NewServer(hl.WithCloseHandler())
+	defer s.Close()
+
+	client := s.Client()
+
+	res, err := client.Get(s.URL)
+	assert.NoError(err)
+	assert.Equal(401, res.StatusCode)
+
+	req, _ := http.NewRequest("GET", s.URL, nil)
+	req.SetBasicAuth("joe", "wrong password")
+	res, err = client.Do(req)
+	assert.NoError(err)
+	assert.Equal(401, res.StatusCode)
+
+	req, _ = http.NewRequest("GET", s.URL, nil)
+	req.SetBasicAuth("joe", "secret")
+	res, err = client.Do(req)
+	assert.NoError(err)
+	assert.Equal(404, res.StatusCode) // auth should work and it should fall through to 404
+
+}
